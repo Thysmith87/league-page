98577a5064df7759ee09083a4c8b214f181cd07f
Thysmith87 <60322933+Thysmith87@users.noreply.github.com>
2025-08-11 16:20:17 -0700

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -5,7 +5,6 @@ export const leagueName = "WeAreTheMeanGirls"; // your league name
 export const dues = 20; // (optional) used in template constitution page
 export const dynasty = false; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
-export const draftid = "1182840756039831553"; // League Draft ID. 
 
 /*   STEP 2   */
 export const homepageText = `
