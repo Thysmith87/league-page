2ece46b636a118121556640b8fe5ebdd057ec0b7
Thysmith87 <60322933+Thysmith87@users.noreply.github.com>
2025-08-12 16:12:59 -0700

    Create manualKeepers.js

---
+++ src/lib/utils/helperFunctions/manualKeepers.js
@@ -0,0 +1,10 @@
+export const manualKeepers = {
+  "team1_id": [
+    { playerName: "Patrick Mahomes", round: 3 },
+    { playerName: "Christian McCaffrey", round: 1 }
+  ],
+  "team2_id": [
+    { playerName: "Justin Jefferson", round: 2 }
+  ],
+  // ...etc for each league member
+};
