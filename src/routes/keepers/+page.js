ed9b348dec16b90b363302e91f2821290cdab32c
Thysmith87 <60322933+Thysmith87@users.noreply.github.com>
2025-08-11 22:13:05 -0700

    Update +page.js

--- src/routes/keepers/+page.js
+++ src/routes/keepers/+page.js
@@ -1,10 +1,12 @@
 // src/routes/+page.js
-import { getUpcomingDraft, leagueID } from '$lib/utils/helper';
+
+const LEAGUE_ID = '1182840756039831552';
+const DRAFT_ID = '1182840756039831553';
 
 export async function load() {
   const [rosters, draft, players, adp] = await Promise.all([
-    fetch(`https://api.sleeper.app/v1/league/${leagueID}/rosters`).then(r => r.json()),
-    fetch(`https://api.sleeper.app/v1/draft/${getUpcomingDraft}/picks`).then(r => r.json()),
+    fetch(`https://api.sleeper.app/v1/league/${LEAGUE_ID}/rosters`).then(r => r.json()),
+    fetch(`https://api.sleeper.app/v1/draft/${DRAFT_ID}/picks`).then(r => r.json()),
     fetch(`https://api.sleeper.app/v1/players/nfl`).then(r => r.json()),
     fetch(`https://fantasyfootballcalculator.com/api/v1/adp/half-ppr?teams=12`).then(r => r.json())
   ]);
