37fb7d034a2cbb356dfc0317bf0ff28814dce14e
Thysmith87 <60322933+Thysmith87@users.noreply.github.com>
2025-08-13 10:24:36 -0700

    Update components.js

--- src/lib/components.js
+++ src/lib/components.js
@@ -4,6 +4,7 @@ import News from './News/index.svelte';
 import Resources from './Resources.svelte';
 import Awards from './Awards/Awards.svelte';
 import Rosters from './Rosters/Rosters.svelte';
+import Keepers from './Keepers/Keepers.svelte';
 import Rivalry from './Rivalry/index.svelte';
 import Transactions from './Transactions/Transactions.svelte';
 import TransactionsPage from './Transactions/TransactionsPage.svelte';
@@ -40,4 +41,5 @@ export {
     Posts,
     FullPost,
     Standings,
+    Keepers,
 };
