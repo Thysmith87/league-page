b44f0c36bd924df97e19f02cd4ec5034f3f63e56
Thysmith87 <60322933+Thysmith87@users.noreply.github.com>
2025-08-13 09:21:26 -0700

    Update +page.svelte

--- src/routes/keepers/+page.svelte
+++ src/routes/keepers/+page.svelte
@@ -1,37 +1,25 @@
-<script>
-  export let data;
-</script>
-
-<h1>Keeper Options</h1>
-{#if data.keepers && data.keepers.length}
- <table>
-  <thead>
+<tbody>
+  {#each data.keepers as k}
     <tr>
-      <th>Owner</th>
-      <th>Player</th>
-      <th>Pos</th>
-      <th>Team</th>
-      <th>Round</th>
-      <th>Keeper Cost</th>
-      <th>ADP</th>
-      <th>Eligibility</th>
+      <td>{k.owner}</td>
+      <td>{k.player}</td>
+      <td>{k.position}</td>
+      <td>{k.team}</td>
+      <td>{k.draftRound}</td>
+      <td>{k.keeperCost}</td>
+      <td>{k.adp}</td>
+      <td>
+        <span
+          style="
+            display: inline-block;
+            width: 12px;
+            height: 12px;
+            border-radius: 50%;
+            background-color: {k.eligibility};
+          "
+          title={k.eligibility}
+        ></span>
+      </td>
     </tr>
-  </thead>
-  <tbody>
-    {#each data.keepers as k}
-      <tr>
-        <td>{k.owner}</td>
-        <td>{k.player}</td>
-        <td>{k.position}</td>
-        <td>{k.team}</td>
-        <td>{k.draftRound}</td>
-        <td>{k.keeperCost}</td>
-        <td>{k.adp}</td>
-        <td>{k.eligibility}</td>
-      </tr>
-    {/each}
-  </tbody>
-</table>
-{:else}
-  <p>No keeper data found.</p>
-{/if}
+  {/each}
+</tbody>
