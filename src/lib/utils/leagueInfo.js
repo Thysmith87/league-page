b6aa3f5d631513da81fe0de9cf5a4a93b576c955
Thysmith87 <60322933+Thysmith87@users.noreply.github.com>
2026-09-01 21:10:56 -0700

    Update league ID in leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "1182840756039831552"; // your league ID
+export const leagueID = "1389755386164170752"; // your league ID
 export const leagueName = "WeAreTheMeanGirls"; // your league name
 export const dues = 20; // (optional) used in template constitution page
 export const dynasty = false; // true for dynasty leagues, false for redraft and keeper
